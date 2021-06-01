f02f300b31a7fa94609bbb2ae2675fbe3d9b228f
Daniel Garcia <danielgarciaabello@gmail.com>
2021-05-31 19:41:45 -0500

    confirmando cambios respecto a los comentarios

--- src/Logica/fcliente.java
+++ src/Logica/fcliente.java
@@ -1,12 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Logica;
 
 import Datos.vcliente;
-import Datos.vproducto;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
